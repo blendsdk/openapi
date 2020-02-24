ebba72e4147681e3f771f2d0e35b26e137b6f798
Gevik Babakhani <gevik@blendjs.com>
2020-02-24 20:59:24 +0100

    Added IParameterObject

--- src/v3.ts
+++ src/v3.ts
@@ -30,6 +30,146 @@ const a130: IOAuthFlowObject;
 const a131: ISecurityRequirementObject;
 
 /**
+ * Describes a single operation parameter.
+ * A unique parameter is defined by a combination of a name and location.
+ *
+ * @export
+ * @interface IParameterObject
+ */
+export interface IParameterObject {
+    /**
+     * The name of the parameter. Parameter names are case sensitive.
+     *
+     * If in is "path", the name field MUST correspond to a template expression
+     * occurring within the path field in the Paths Object. See Path Templating
+     * for further information.
+     *
+     * If in is "header" and the name field is "Accept", "Content-Type" or "Authorization",
+     * the parameter definition SHALL be ignored. For all other cases, the name corresponds
+     * to the parameter name used by the in property.
+     *
+     * @type {string}
+     * @memberof IParameterObject
+     */
+    name: string;
+    /**
+     * The location of the parameter. Possible values are "query",
+     * "header", "path" or "cookie".
+     *
+     * @type {string}
+     * @memberof IParameterObject
+     */
+    in: string;
+    /**
+     * A brief description of the parameter. This could contain examples of
+     * use. CommonMark syntax MAY be used for rich text representation.
+     *
+     * @type {string}
+     * @memberof IParameterObject
+     */
+    description?: string;
+    /**
+     * Determines whether this parameter is mandatory. If the parameter location
+     * is "path", this property is REQUIRED and its value MUST be true.
+     * Otherwise, the property MAY be included and its default value is false.
+     *
+     * @type {boolean}
+     * @memberof IParameterObject
+     */
+    required?: boolean;
+    /**
+     * Specifies that a parameter is deprecated and SHOULD be
+     * transitioned out of usage. Default value is false.
+     *
+     * @type {boolean}
+     * @memberof IParameterObject
+     */
+    deprecated?: boolean;
+    /**
+     * Sets the ability to pass empty-valued parameters. This is valid only
+     * for query parameters and allows sending a parameter with an empty
+     * value. Default value is false. If style is used, and if behavior
+     * is n/a (cannot be serialized), the value of allowEmptyValue SHALL
+     * be ignored. Use of this property is NOT RECOMMENDED, as it is likely
+     * to be removed in a later revision.
+     *
+     * @type {boolean}
+     * @memberof IParameterObject
+     */
+    allowEmptyValue?: boolean;
+    /**
+     * Describes how the parameter value will be serialized depending on the
+     * type of the parameter value. Default values (based on value of in):
+     * for query - form; for path - simple; for header - simple;
+     * for cookie - form.
+     *
+     * @type {string}
+     * @memberof IParameterObject
+     */
+    style?: string;
+    /**
+     * When this is true, parameter values of type array or object generate
+     * separate parameters for each value of the array or key-value pair
+     * of the map. For other types of parameters this property has no
+     * effect. When style is form, the default value is true. For all
+     * other styles, the default value is false.
+     *
+     * @type {boolean}
+     * @memberof IParameterObject
+     */
+    explode?: boolean;
+    /**
+     * Determines whether the parameter value SHOULD allow reserved characters,
+     * as defined by RFC3986 :/?#[]@!$&'()*+,;= to be included without percent-encoding.
+     * This property only applies to parameters with an in value of query.
+     * The default value is false.
+     *
+     * @type {boolean}
+     * @memberof IParameterObject
+     */
+    allowReserved: boolean;
+    /**
+     * The schema defining the type used for the parameter.
+     *
+     * @type {(ISchemaObject | IReferenceObject)}
+     * @memberof IParameterObject
+     */
+    schema?: ISchemaObject | IReferenceObject;
+    /**
+     * Example of the parameter's potential value. The example SHOULD match the
+     * specified schema and encoding properties if present. The example field
+     * is mutually exclusive of the examples field. Furthermore, if referencing
+     * a schema that contains an example, the example value SHALL override the
+     * example provided by the schema. To represent examples of media types
+     * that cannot naturally be represented in JSON or YAML, a string value can
+     * contain the example with escaping where necessary.
+     *
+     * @type {*}
+     * @memberof IParameterObject
+     */
+    example?: any;
+    /**
+     * Examples of the parameter's potential value. Each example SHOULD contain a value
+     * in the correct format as specified in the parameter encoding. The examples
+     * field is mutually exclusive of the example field. Furthermore, if referencing
+     * a schema that contains an example, the examples value SHALL override
+     * the example provided by the schema.
+     *
+     * @type {(IDictionaryOf<IExampleObject | IReferenceObject>)}
+     * @memberof IParameterObject
+     */
+    examples?: IDictionaryOf<IExampleObject | IReferenceObject>;
+    /**
+     * A map containing the representations for the parameter. The key is the media
+     * type and the value describes it. The map MUST only contain one entry.
+     *
+     * @type {IDictionaryOf<IMediaTypeObject>}
+     * @memberof IParameterObject
+     */
+    content?: IDictionaryOf<IMediaTypeObject>;
+}
+
+/**
  * The Schema Object allows the definition of input and output data types.
  * These types can be objects, but also primitives and arrays. This object is
  * an extended subset of the JSON Schema Specification Wright Draft 00.
