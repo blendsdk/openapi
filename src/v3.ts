881947e5c39f3cec1a49567f94c4c1ce5eb42901
Gevik Babakhani <g.babakhani@truesoftware.nl>
2020-02-24 18:06:56 +0100

    Added IXMLObject

--- src/v3.ts
+++ src/v3.ts
@@ -30,6 +30,60 @@ const a130: IOAuthFlowObject;
 const a131: ISecurityRequirementObject;
 
 /**
+ * A metadata object that allows for more fine-tuned XML model definitions.
+ * When using arrays, XML element names are not inferred 
+ * (for singular/plural forms) and the name property SHOULD be used to 
+ * add that information. See examples for expected behavior.
+ *
+ * @export
+ * @interface IXMLObject
+ */
+export interface IXMLObject {
+    /**
+     * Replaces the name of the element/attribute used for the described
+     * schema property. When defined within items, it will affect the name
+     * of the individual XML elements within the list. When defined alongside
+     * type being array (outside the items), it will affect the wrapping
+     * element and only if wrapped is true. If wrapped is false, it will be ignored.
+     *
+     * @type {string}
+     * @memberof IXMLObject
+     */
+    name?: string;
+    /**
+     * The URI of the namespace definition. Value MUST be in the form of an absolute URI.
+     *
+     * @type {string}
+     * @memberof IXMLObject
+     */
+    namespace?: string;
+    /**
+     * The prefix to be used for the name.
+     *
+     * @type {string}
+     * @memberof IXMLObject
+     */
+    prefix?: string;
+    /**
+     * Declares whether the property definition translates to an attribute instead
+     * of an element. Default value is false.
+     *
+     * @type {boolean}
+     * @memberof IXMLObject
+     */
+    attribute?: boolean;
+    /**
+     * MAY be used only for an array definition. Signifies whether the array is
+     * wrapped (for example, <books><book/><book/></books>) or unwrapped (<book/><book/>).
+     * Default value is false. The definition takes effect only when definedalongside type being array (outside the items).
+     *
+     * @type {boolean}
+     * @memberof IXMLObject
+     */
+    wrapped?: boolean;
+}
+
+/**
  * Each Media Type Object provides schema and examples for the
  * media type identified by its key.
  *
