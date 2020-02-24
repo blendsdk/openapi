5d245512617ddb158c0852b161fd759d40f01728
Gevik Babakhani <g.babakhani@truesoftware.nl>
2020-02-24 18:10:58 +0100

    Added IResponseObject

--- src/v3.ts
+++ src/v3.ts
@@ -30,6 +30,52 @@ const a130: IOAuthFlowObject;
 const a131: ISecurityRequirementObject;
 
 /**
+ * Describes a single response from an API Operation, including design-time,
+ * static links to operations based on the response.
+ *
+ * @export
+ * @interface IResponseObject
+ */
+export interface IResponseObject {
+    /**
+     * A short description of the response. CommonMark syntax
+     * MAY be used for rich text representation.
+     *
+     * @type {string}
+     * @memberof IResponseObject
+     */
+    description: string;
+    /**
+     * Maps a header name to its definition. RFC7230 states header 
+     * names are case insensitive. If a response header is defined
+     * with the name "Content-Type", it SHALL be ignored.
+     *
+     * @type {(IDictionaryOf<IHeaderObject | IReferenceObject>)}
+     * @memberof IResponseObject
+     */
+    headers?: IDictionaryOf<IHeaderObject | IReferenceObject>;
+    /**
+     * A map containing descriptions of potential response payloads.
+     * The key is a media type or media type range and the value describes
+     * it. For responses that match multiple keys, only the most specific
+     * key is applicable. e.g. text/plain overrides text/*
+     *
+     * @type {IDictionaryOf<IMediaTypeObject>}
+     * @memberof IResponseObject
+     */
+    content?: IDictionaryOf<IMediaTypeObject>;
+    /**
+     * A map of operations links that can be followed from the response.
+     * The key of the map is a short name for the link, following the naming
+     * constraints of the names for Component Objects.
+     *
+     * @type {(IDictionaryOf<ILinkObject | IReferenceObject>)}
+     * @memberof IResponseObject
+     */
+    links?: IDictionaryOf<ILinkObject | IReferenceObject>;
+}
+
+/**
  * A metadata object that allows for more fine-tuned XML model definitions.
  * When using arrays, XML element names are not inferred 
  * (for singular/plural forms) and the name property SHOULD be used to 
